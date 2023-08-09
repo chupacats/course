a37c8f70db611682f0b0a8a1b46e30f2001fc1f6
chupacats <anastasiya.krn@gmail.com>
2023-08-09 08:53:11 +0300

    My first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a = prompt('Один из последних просмотренных фильмов', ''),
+  b = prompt('На сколько его оцените?', ''),
+  c = prompt('Один из последних просмотренных фильмов', ''),
+  d = prompt('На сколько его оцените?', '');
+
+personalMovieDB.movies[a] = b; //почему
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
